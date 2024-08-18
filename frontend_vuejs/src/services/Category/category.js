ad4e0bb3addb29428b11ceb77e90378d9d333f78
linh-nv <kiaisoft.linh.nguyen@gmail.com>
2024-08-18 14:35:56 +0700

    (hot fix) add missing files

---
+++ frontend_vuejs/src/services/Category/category.js
@@ -0,0 +1,30 @@
+import { apiService } from "@/services/apiService";
+import { CATEGORY_ENDPONIT } from "@/utils/apisDomain";
+
+const endpoint = CATEGORY_ENDPONIT;
+
+export const categoryService = {
+  getAll(page) {
+    return apiService.getAll(endpoint, page);
+  },
+
+  create(data) {
+    return apiService.create(endpoint, data);
+  },
+
+  update(id, data) {
+    return apiService.update(endpoint, id, data);
+  },
+
+  find(id) {
+    return apiService.find(endpoint, id);
+  },
+
+  delete(id) {
+    return apiService.delete(endpoint, id);
+  },
+
+  pluck() {
+    return apiService.pluck(endpoint);
+  },
+};
