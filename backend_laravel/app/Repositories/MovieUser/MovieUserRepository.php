b6ee6a768aa76bbf62ec73f126d499642ec1fda0
linh-nv <kiaisoft.linh.nguyen@gmail.com>
2024-10-03 14:20:24 +0700

    (feature) complete feature favorite movie

--- backend_laravel/app/Repositories/MovieUser/MovieUserRepository.php
+++ backend_laravel/app/Repositories/MovieUser/MovieUserRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repositories\MovieUser;
 
 use App\Repositories\BaseRepository;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Collection;
 
 class MovieUserRepository extends BaseRepository implements MovieUserRepositoryInterface
@@ -20,8 +21,14 @@ class MovieUserRepository extends BaseRepository implements MovieUserRepositoryI
     public function getMovies(int $userId): ?Collection
     {
 
-        return $this->_model         
-        ->where('user_id', $userId)
-        ->pluck('movie_id');
+        return $this->_model
+            ->where('user_id', $userId)
+            ->pluck('movie_id');
+    }
+
+    public function findByMovieUser(int $movieId, int $userId): ?Model
+    {
+
+        return $this->_model->where('movie_id', $movieId)->where('user_id', $userId)->first();
     }
 }
