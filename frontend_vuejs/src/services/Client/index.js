17eaf727133cf656ba9e77296455e5e5cc35282e
linh-nv <kiaisoft.linh.nguyen@gmail.com>
2024-10-05 08:18:54 +0700

    modify api path

--- frontend_vuejs/src/services/Client/index.js
+++ frontend_vuejs/src/services/Client/index.js
@@ -1,8 +1,20 @@
 import { apiClientService } from "./apiClientService";
 import {
+  CATEGORY_MOVIE_ENDPONIT,
+  CHECK_FAVORITE_ENDPONIT,
   CLIENT_API_BASE_URL,
+  CLIENT_MOVIE_ENDPONIT,
+  COUNTRY_MOVIE_ENDPONIT,
+  CREATE_VIEW_ENDPONIT,
+  FILTER_ENDPOINT,
+  FILTER_OPTION_ENDPOINT,
+  GENRE_MOVIE_ENDPONIT,
   HEADER_ENDPONIT,
+  MOVIE_COMMENT_ENDPONIT,
+  MOVIE_FAVORITE_ENDPONIT,
+  MOVIE_TOP_ENDPONIT,
   SEARCH_ENDPONIT,
+  SILDER_ENDPONIT,
 } from "@/utils/apisDomain";
 
 const endpoint = CLIENT_API_BASE_URL;
@@ -20,51 +32,51 @@ export const clientService = {
     return apiClientService.get(endpoint);
   },
   getMovieFilter(params) {
-    return apiClientService.get(endpoint + "/filter", params);
+    return apiClientService.get(endpoint + FILTER_ENDPOINT, params);
   },
   getFilterOption() {
-    return apiClientService.get(endpoint + "/filter-option");
+    return apiClientService.get(endpoint + FILTER_OPTION_ENDPOINT);
   },
   getSilier() {
-    return apiClientService.get(endpoint + "/slider");
+    return apiClientService.get(endpoint + SILDER_ENDPONIT);
   },
   getTrending() {
-    return apiClientService.get(endpoint + "/movie-top");
+    return apiClientService.get(endpoint + MOVIE_TOP_ENDPONIT);
   },
   getCategory(slug) {
-    return apiClientService.find(endpoint + "/category", slug);
+    return apiClientService.find(endpoint + CATEGORY_MOVIE_ENDPONIT, slug);
   },
   getCountry(slug) {
-    return apiClientService.find(endpoint + "/country", slug);
+    return apiClientService.find(endpoint + COUNTRY_MOVIE_ENDPONIT, slug);
   },
   getGenre(slug) {
-    return apiClientService.find(endpoint + "/genre", slug);
+    return apiClientService.find(endpoint + GENRE_MOVIE_ENDPONIT, slug);
   },
   getPaginate(url) {
     return apiClientService.getByUrl(url);
   },
   getMovie(slug) {
-    return apiClientService.find(endpoint + "/movie", slug);
+    return apiClientService.find(endpoint + CLIENT_MOVIE_ENDPONIT, slug);
   },
   getComments(slug) {
-    return apiClientService.find(endpoint + "/comment", slug);
+    return apiClientService.find(endpoint + MOVIE_COMMENT_ENDPONIT, slug);
   },
   createComments(data) {
-    return apiClientService.create(endpoint + "/comment", data);
+    return apiClientService.create(endpoint + MOVIE_COMMENT_ENDPONIT, data);
   },
   deleteComments(id) {
-    return apiClientService.delete(endpoint + "/comment", id);
+    return apiClientService.delete(endpoint + MOVIE_COMMENT_ENDPONIT, id);
   },
   getFavorite(id) {
-    return apiClientService.find(endpoint + "/favorite", id);
+    return apiClientService.find(endpoint + MOVIE_FAVORITE_ENDPONIT, id);
   },
   createFavorite(data) {
-    return apiClientService.create(endpoint + "/favorite", data);
+    return apiClientService.create(endpoint + MOVIE_FAVORITE_ENDPONIT, data);
   },
   checkExistFavorite(id) {
-    return apiClientService.find(endpoint + "/favorite/exist", id);
+    return apiClientService.find(endpoint + CHECK_FAVORITE_ENDPONIT, id);
   },
   createView(id) {
-    return apiClientService.find(endpoint + "/create-view", id);
+    return apiClientService.find(endpoint + CREATE_VIEW_ENDPONIT, id);
   },
 };
